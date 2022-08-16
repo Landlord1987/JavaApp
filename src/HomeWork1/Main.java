4b15cdfae6a603d93e4768d925ef9803eb574a43
DenisKrinitsyn <skylinekrin@gmail.com>
2022-08-16 22:24:16 +0500

    The Second Task

--- src/HomeWork1/Main.java
+++ src/HomeWork1/Main.java
@@ -2,6 +2,18 @@ package HomeWork1;
 
 public class Main {
     public static void main(String[] args) {
+        byte a = 1;
+        short b = 130;
+        int c = 33000;
+        long d = 50000;
+
+        String text = "Привет";
+        Boolean key = true;
+
+        float e = 24.45f;
+        double f = 45.65;
+
+        char A = 'a';
 
     }
 }
