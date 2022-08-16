57e34233d10c7c7d496bbcf5c3d842329a9305a0
DenisKrinitsyn <skylinekrin@gmail.com>
2022-08-16 22:55:19 +0500

    The Third Task

--- src/HomeWork1/Main.java
+++ src/HomeWork1/Main.java
@@ -1,11 +1,13 @@
 package HomeWork1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        byte a = 1;
-        short b = 130;
-        int c = 33000;
-        long d = 50000;
+        byte aa = 1;
+        short bb = 130;
+        int cc = 33000;
+        long dd = 50000;
 
         String text = "Привет";
         Boolean key = true;
@@ -15,5 +17,21 @@ public class Main {
 
         char A = 'a';
 
+        float a, b, c, d;
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите a: "); a = console.nextFloat();
+        System.out.println("Введите b: "); b = console.nextFloat();
+        System.out.println("Введите c: "); c = console.nextFloat();
+        System.out.println("Введите d: "); d = console.nextFloat();
+
+        float res = myMethod(a,b,c,d);
+        System.out.println("Результат: " + res);
+
+
     }
+
+    static float myMethod (float a, float b, float c, float d){
+        return a * (b + (c/d));
+    }
+
 }
