36b10b76b74bbf68a16d6c37d38fb71b04f62355
DenisKrinitsyn <skylinekrin@gmail.com>
2022-08-16 23:36:06 +0500

    The Fifth Task

--- src/HomeWork1/Main.java
+++ src/HomeWork1/Main.java
@@ -1,6 +1,5 @@
 package HomeWork1;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -25,15 +24,18 @@ public class Main {
         System.out.println("¬ведите c: "); c = console.nextFloat();
         System.out.println("¬ведите d: "); d = console.nextFloat();
 
-        float res = myMethod(a,b,c,d);
-        System.out.println("–езультат: " + res);
+        System.out.println("–езультат: " + myMethod(a,b,c,d));
 
         int y,z;
         System.out.println("¬ведите y: "); y = console.nextInt();
         System.out.println("¬ведите z: "); z = console.nextInt();
 
-        boolean res2 = myMethod2(y,z);
-        System.out.println("Ёто " + res2 + " ,что результат y+z лежит в пределах от 10 до 20");
+        System.out.println("Ёто " + myMethod2(y,z) + " ,что результат y+z лежит в пределах от 10 до 20");
+
+        int w;
+        System.out.println("¬ведите w: "); w = console.nextInt();
+
+        myMethod3(w);
 
 
     }
@@ -44,6 +46,12 @@ public class Main {
 
     static boolean myMethod2(int y, int z){
         return (y + z) >= 10 && (y + z) <= 20;
+    }
+
+    static void myMethod3 (int w){
+        if (w >= 0) {System.out.println("„исло положительное");}
+        else System.out.println("„исло отрицательное");
+
     }
 
 }
